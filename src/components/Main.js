3ae7389e44efc1fcbb9cac5f31da581e9c9ec0d1
Neha <nehatualea@gmail.com>
2023-09-20 01:30:56 +0530

    displayed : Pricing Card in main section

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PricingCard from './PricingCard';
+
+const Main = () => {
+  return (
+    <main>
+        <h1>Choose a plan that's just right for you!</h1>
+      <PricingCard />
+    </main>
+  );
+};
+
+export default Main;
