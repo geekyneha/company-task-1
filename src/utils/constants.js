4f50366a8a1d67ec0712504f19aca77e05b91248
Neha <nehatualea@gmail.com>
2023-09-19 20:45:20 +0530

    Added: User Profile  URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,5 @@ export const APP_LOGO_URL =
   'https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/uajc6dvtetkgeclvmaku';
 export const COMPANY_LOGO =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy09SzlAEVUCweVgzbLIPLfQL8uwEJN-2nMw&usqp=CAU';
+export const USER_PROFILE =
+  'https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/short/linkedin-profile-picture-maker/dummy_image/thumb/001.webp';
