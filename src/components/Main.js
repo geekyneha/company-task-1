c6fcabba3dfbd5a78669426ab107626d9a6eb1e1
Neha <nehatualea@gmail.com>
2023-09-20 04:35:36 +0530

    added: small text

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PricingCard from './PricingCard';
 import SubscriptionCard from './SubscriptionCard';
+import style from '../styles/Main.module.css';
 
 const Main = () => {
   return (
     <main>
-      <h1>Choose a plan that's just right for you!</h1>
+      <h1 className={style['heading']}>
+        Choose a plan that's just right for you!
+      </h1>
       <PricingCard />
       <SubscriptionCard />
+      <div className={style['text']}>
+        <div></div>
+        <div className={style['small-text']}>
+          *some unique features are provided as add-ons with individual plans
+          for each features
+        </div>
+      </div>
     </main>
   );
 };
