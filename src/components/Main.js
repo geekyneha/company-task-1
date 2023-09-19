f13a280edbd5d9a23e4d544afc404956d37d14c9
Neha <nehatualea@gmail.com>
2023-09-20 02:36:53 +0530

    Added:  Subscription card

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PricingCard from './PricingCard';
+import SubscriptionCard from './SubscriptionCard';
 
 const Main = () => {
   return (
     <main>
-        <h1>Choose a plan that's just right for you!</h1>
+      <h1>Choose a plan that's just right for you!</h1>
       <PricingCard />
+      <SubscriptionCard />
     </main>
   );
 };
