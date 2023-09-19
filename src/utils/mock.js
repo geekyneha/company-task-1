7143392b659617156d6c13afd92562ab6dd7095b
Neha <nehatualea@gmail.com>
2023-09-19 22:56:44 +0530

    added : navigations mock data

---
+++ src/utils/mock.js
@@ -0,0 +1,33 @@
+import icon1 from '../assets/dashboard.png';
+import icon2 from '../assets/Perks.png';
+import icon3 from '../assets/puzzle-piece.png';
+import icon4 from '../assets/faq.png';
+import icon5 from '../assets/Feedback_Hub_Icon.png';
+
+export const navigations = [
+  {
+    id: 1,
+    name: 'Dashboard',
+    icon: icon1,
+  },
+  {
+    id: 2,
+    name: 'Perks',
+    icon: icon2,
+  },
+  {
+    id: 3,
+    name: 'Addons',
+    icon: icon3,
+  },
+  {
+    id: 4,
+    name: 'FAQ',
+    icon: icon4,
+  },
+  {
+    id: 5,
+    name: 'Support',
+    icon: icon5,
+  },
+];
