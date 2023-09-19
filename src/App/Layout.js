0f6b31a3e868d628bd4f2993865d10299d5b50f0
Neha <nehatualea@gmail.com>
2023-09-20 01:27:39 +0530

    created: layout and styled

--- src/App/Layout.js
+++ src/App/Layout.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
 import { LeftSideBar } from '../components/LeftSideBar';
+import style from '../styles/Layout.module.css';
+import Main from '../components/Main';
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <LeftSideBar />
-      
+      <div className={style['layout']}>
+        <div className={style['left-side-bar']}>
+          <LeftSideBar />
+        </div>
+        <div className={style['main-section']}>
+          <Main />
+        </div>
+        <div className={style['right-side-bar']}></div>
+      </div>
     </div>
   );
 };
