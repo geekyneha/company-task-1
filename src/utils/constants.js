8fa8240e126df7186f5a8f3c0f8b70c149972aeb
Neha <nehatualea@gmail.com>
2023-09-19 19:58:45 +0530

    Added: URLs

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const APP_LOGO_URL =
+  'https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/uajc6dvtetkgeclvmaku';
+export const COMPANY_LOGO =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy09SzlAEVUCweVgzbLIPLfQL8uwEJN-2nMw&usqp=CAU';
