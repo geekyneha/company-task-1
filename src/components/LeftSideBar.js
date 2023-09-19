aaa4f1e8a6d192be17f89e55124594513207eae2
Neha <nehatualea@gmail.com>
2023-09-19 22:58:16 +0530

    added: Menu and Logout component

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import User from './User'
+import Menu from './Menu'
+import Logout from './Logout'
+import style from '../styles/LeftSideBar.module.css'
 
 export const LeftSideBar = () => {
   return (
     <div>
         <User />
+        <Menu />
+        <div className={style["logout-container"]}>
+        <Logout />
+        </div>
     </div>
   )
 }
