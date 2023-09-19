5532e59a6d87ce7522d795701f8020faec916ce2
Neha <nehatualea@gmail.com>
2023-09-19 20:48:55 +0530

    Added: Leftside bar

--- src/App/Layout.js
+++ src/App/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import { LeftSideBar } from '../components/LeftSideBar';
 
 const Layout = () => {
   return (
     <div>
       <Header />
+      <LeftSideBar />
+      
     </div>
   );
 };
